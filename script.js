05f0011d89c5d6ec0ea63f370277061ee2c14442
Aris Anninga <88113610+AEAnninga@users.noreply.github.com>
2021-09-03 10:04:57 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("Hallo Winc Academy Studenten");
+// Dit is een grote som
+/*
+comments zie je niet terug in de concole of terminal omdat: 
+Javascript negeert comments, dus ook code in comments wordt genegeerd
+en zie je dus niet terug in de uitkomst
+*/
+/* 
+console.log(1230941 * 1823794);
+console.log(637263 / 54);
+*/
